ffb8ab4b8a33f75cb6ba01014286d64a014cb387
Vanilla <nicolevillalbaviellard@outlook.com>
2020-04-14 19:43:57 -0500

    primeras interfaces

--- Parqueadero/src/parqueadero/Parqueadero.java
+++ Parqueadero/src/parqueadero/Parqueadero.java
@@ -14,7 +14,7 @@ public class Parqueadero {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int a;// TODO code application logic here
+        // TODO code application logic here
     }
     
 }
